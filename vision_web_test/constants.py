774251f38831b4ac5d1480563486fb725b07d44f
Mark Lysenko <oomarkizoo@mail.ru>
2021-01-13 22:23:02 +0300

    Adds a file with project constants

---
+++ vision_web_test/constants.py
@@ -0,0 +1,6 @@
+ELEMENTS_TEMPLATES = {
+    "BlockWithImageAndList": "image_and_list",
+    "BlockWithFourTexts": "four_texts",
+    "BlockWithTitleTextAndButton": "title_text_and_button",
+    "BlockWithTitleTextImageAndButtons": "title_text_image_and_button",
+}
