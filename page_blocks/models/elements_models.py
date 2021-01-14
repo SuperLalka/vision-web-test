d3332cad879e9ebf8dd35e7234d4bdfcc4bcff7d
Mark Lysenko <oomarkizoo@mail.ru>
2021-01-14 18:13:26 +0300

    Makes changes to the project models
    - Adds a new default user model
    - Makes all element model fields optional
    - The template name for element obtained by the method

--- page_blocks/models/elements_models.py
+++ page_blocks/models/elements_models.py
@@ -4,54 +4,69 @@ from django.db import models
 from vision_web_test import constants
 
 
-class BlockWithImageAndList(models.Model):
-    template_name = models.CharField(max_length=30, editable=False,
-                                     default=constants.ELEMENTS_TEMPLATES[__qualname__])
+optional = {'null': True, 'blank': True}
 
-    images = models.ImageField(upload_to="images")
-    tags = ArrayField(models.CharField(max_length=50), blank=True, size=5)
+
+class BlockWithImageAndList(models.Model):
+    images = models.ImageField(upload_to="images", **optional)
+    list_items = ArrayField(models.CharField(max_length=50), **optional, size=5)
 
     def __str__(self):
         return f'{self.id} / BlockWithImageAndList'
 
+    def get_template_name(self):
+        return constants.ELEMENTS_TEMPLATES[self.__class__.__name__]
+
+    class Meta:
+        db_table = "block_image_and_list"
+
 
 class BlockWithFourTexts(models.Model):
-    template_name = models.CharField(max_length=30, editable=False,
-                                     default=constants.ELEMENTS_TEMPLATES[__qualname__])
-
-    top_left_text = models.CharField(max_length=255, null=True, blank=True)
-    top_right_text = models.CharField(max_length=255, null=True, blank=True)
-    bottom_left_text = models.CharField(max_length=255, null=True, blank=True)
-    bottom_right_text = models.CharField(max_length=255, null=True, blank=True)
+    top_left_text = models.CharField(max_length=255, **optional)
+    top_right_text = models.CharField(max_length=255, **optional)
+    bottom_left_text = models.CharField(max_length=255, **optional)
+    bottom_right_text = models.CharField(max_length=255, **optional)
 
     def __str__(self):
         return f'{self.id} / BlockWithFourTexts'
 
+    def get_template_name(self):
+        return constants.ELEMENTS_TEMPLATES[self.__class__.__name__]
+
+    class Meta:
+        db_table = "block_four_text"
+
 
 class BlockWithTitleTextAndButton(models.Model):
-    template_name = models.CharField(max_length=30, editable=False,
-                                     default=constants.ELEMENTS_TEMPLATES[__qualname__])
-
-    title = models.CharField(max_length=100, null=True, blank=True)
-    text = models.TextField()
-    button_text = models.CharField(max_length=30)
-    button_action = models.CharField(max_length=30, null=True, blank=True)
+    title = models.CharField(max_length=100, **optional)
+    text = models.TextField(**optional)
+    button_text = models.CharField(max_length=30, **optional)
+    button_action = models.CharField(max_length=30, **optional)
 
     def __str__(self):
         return f'{self.id} / BlockWithTitleTextAndButton'
 
+    def get_template_name(self):
+        return constants.ELEMENTS_TEMPLATES[self.__class__.__name__]
+
+    class Meta:
+        db_table = "block_title_text_and_button"
+
 
 class BlockWithTitleTextImageAndButtons(models.Model):
-    template_name = models.CharField(max_length=30, editable=False,
-                                     default=constants.ELEMENTS_TEMPLATES[__qualname__])
-
-    images = models.ImageField(upload_to="images")
-    title = models.CharField(max_length=100, null=True, blank=True)
-    text = models.TextField()
-    first_button_text = models.CharField(max_length=30)
-    first_button_action = models.CharField(max_length=30, null=True, blank=True)
-    second_button_text = models.CharField(max_length=30)
-    second_button_action = models.CharField(max_length=30, null=True, blank=True)
+    images = models.ImageField(upload_to="images", **optional)
+    title = models.CharField(max_length=100, **optional)
+    text = models.TextField(**optional)
+    first_button_text = models.CharField(max_length=30, **optional)
+    first_button_action = models.CharField(max_length=30, **optional)
+    second_button_text = models.CharField(max_length=30, **optional)
+    second_button_action = models.CharField(max_length=30, **optional)
 
     def __str__(self):
         return f'{self.id} / BlockWithTitleTextImageAndButtons'
+
+    def get_template_name(self):
+        return constants.ELEMENTS_TEMPLATES[self.__class__.__name__]
+
+    class Meta:
+        db_table = "block_title_text_image_and_buttons"
--- page_blocks/models/models.py
+++ page_blocks/models/models.py
@@ -1,20 +1,38 @@
+from django.contrib.auth.models import AbstractUser
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+
+from vision_web_test import constants
 
 
 BLOCKS_NAME = "Enter a name for the block"
 BLOCKS_SORTING = "Sequence of block pins on a page"
 
+optional = {'null': True, 'blank': True}
+
 
 class Block(models.Model):
-    name = models.CharField(max_length=100, help_text=BLOCKS_NAME)
-    sorting = models.PositiveSmallIntegerField(default=None, help_text=BLOCKS_SORTING)
+    tag = models.SlugField()
+    sorting = models.PositiveSmallIntegerField(default=0, help_text=BLOCKS_SORTING)
     is_active = models.BooleanField(default=True)
 
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveIntegerField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE,
+                                     related_name='content_type_block', editable=False,
+                                     **optional)
+    object_id = models.PositiveIntegerField(editable=False, **optional)
     content_object = GenericForeignKey('content_type', 'object_id')
 
     def __str__(self):
-        return self.name
+        return self.tag
+
+    class Meta:
+        ordering = ['is_active', 'sorting']
+
+
+class User(AbstractUser):
+    user_group = models.CharField(max_length=10, choices=constants.USER_GROUP,
+                                  help_text='Select user group', default='Client')
+
+    def __str__(self):
+        return self.username
