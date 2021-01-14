d6e81e61f62dda0eabcfbea148a77875c9d2fc90
Mark Lysenko <oomarkizoo@mail.ru>
2021-01-14 18:18:32 +0300

    Configures urls for the main page & API for working with JWT

--- vision_web_test/urls.py
+++ vision_web_test/urls.py
@@ -1,21 +1,23 @@
-"""vision_web_test URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic import RedirectView
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+
+tokens_url = [
+    path('', TokenObtainPairView.as_view()),
+    path('refresh/', TokenRefreshView.as_view()),
+    path('verify/', TokenVerifyView.as_view()),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('token/', include(tokens_url)),
+    path('main_page/', include('page_blocks.urls')),
+    path('', RedirectView.as_view(url='/main_page', permanent=True)),
 ]
