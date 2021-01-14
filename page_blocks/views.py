f9f4d07ea5d3f3e0fb572127a2ef0f6851f78113
Mark Lysenko <oomarkizoo@mail.ru>
2021-01-14 18:22:21 +0300

    Adds a view for rendering the main page

--- page_blocks/views.py
+++ page_blocks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from page_blocks.models import (
+    Block
+)
+
+
+def index(request):
+    return render(request, 'index.html', context={
+        'blocks': Block.objects.filter(is_active=True).order_by('sorting')
+    })
