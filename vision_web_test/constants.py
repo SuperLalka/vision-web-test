9cac1ffe78533cba550e2c90d50eb72be4ab84b5
Mark Lysenko <oomarkizoo@mail.ru>
2021-01-14 18:28:40 +0300

    Makes changes to the constants file

--- vision_web_test/constants.py
+++ vision_web_test/constants.py
@@ -1,6 +1,19 @@
 ELEMENTS_TEMPLATES = {
-    "BlockWithImageAndList": "image_and_list",
-    "BlockWithFourTexts": "four_texts",
-    "BlockWithTitleTextAndButton": "title_text_and_button",
-    "BlockWithTitleTextImageAndButtons": "title_text_image_and_button",
+    "BlockWithImageAndList": "image_and_list.html",
+    "BlockWithFourTexts": "four_texts.html",
+    "BlockWithTitleTextAndButton": "title_text_and_button.html",
+    "BlockWithTitleTextImageAndButtons": "title_text_image_and_buttons.html",
 }
+
+
+ELEMENTS_TEMPLATES_CHOICES = [
+    ("BlockWithImageAndList", "image_and_list"),
+    ("BlockWithFourTexts", "four_texts"),
+    ("BlockWithTitleTextAndButton", "title_text_and_button"),
+    ("BlockWithTitleTextImageAndButtons", "title_text_image_and_buttons"),
+]
+
+USER_GROUP = [
+    ('client', 'Client'),
+    ('admin', 'Admin'),
+]
