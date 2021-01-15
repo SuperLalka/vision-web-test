0e93493a208b815392acee29f001cab0ecc8abcd
Mark Lysenko <oomarkizoo@mail.ru>
2021-01-15 16:05:29 +0300

    Adds element models to the admin panel

--- page_blocks/admin.py
+++ page_blocks/admin.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.apps import apps
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
+from django.contrib.postgres.fields import ArrayField
+from django.forms import Textarea
 
 from page_blocks.models import (
     Block,
@@ -17,14 +20,15 @@ class ElementTypeForm(forms.ModelForm):
                                      choices=constants.ELEMENTS_TEMPLATES_CHOICES)
 
     def save(self, *args, **kwargs):
-        super(ElementTypeForm, self).save(*args, **kwargs)
         element_model_name = self.cleaned_data.pop('element_type')
-
         element_model = apps.get_model('page_blocks', element_model_name)
-        element = element_model.objects.create()
-        block = Block.objects.create(**self.cleaned_data,
-                                     content_object=element)
-        return block
+
+        if not self.instance:
+            element = element_model.objects.create()
+            block = Block.objects.create(**self.cleaned_data,
+                                         content_object=element)
+            return block
+        return super(ElementTypeForm, self).save(*args, **kwargs)
 
 
 @admin.register(Block)
@@ -37,7 +41,31 @@ class BlockAdmin(admin.ModelAdmin):
 
 # Elements models
 
-admin.site.register(BlockWithImageAndList)
-admin.site.register(BlockWithFourTexts)
-admin.site.register(BlockWithTitleTextAndButton)
-admin.site.register(BlockWithTitleTextImageAndButtons)
+class BlockInline(GenericTabularInline):
+    model = Block
+    extra = 0
+
+
+class BlockWithFourTextsAdmin(admin.ModelAdmin):
+    inlines = [BlockInline]
+
+
+class BlockWithImageAndListAdmin(admin.ModelAdmin):
+    inlines = [BlockInline]
+    formfield_overrides = {ArrayField: {
+        'widget': Textarea(attrs={'rows': 3})
+    }}
+
+
+class BlockWithTitleTextAndButtonAdmin(admin.ModelAdmin):
+    inlines = [BlockInline]
+
+
+class BlockWithTitleTextImageAndButtonsAdmin(admin.ModelAdmin):
+    inlines = [BlockInline]
+
+
+admin.site.register(BlockWithFourTexts, BlockWithFourTextsAdmin)
+admin.site.register(BlockWithImageAndList, BlockWithImageAndListAdmin)
+admin.site.register(BlockWithTitleTextAndButton, BlockWithTitleTextAndButtonAdmin)
+admin.site.register(BlockWithTitleTextImageAndButtons, BlockWithTitleTextImageAndButtonsAdmin)
